fb9f7e8bdf131a38b2823fd7fda4659e1550e87d
Uthayaprakasam <skuthayaprakasam@gmail.com>
2018-01-28 21:18:32 +0530

    First commit for my selenium code

---
+++ src/main/java/com/qa/lib/TestBase.java
@@ -0,0 +1,10 @@
+package com.qa.lib;
+
+public class TestBase {
+	
+	public void testBase()
+	{
+		System.out.println("test Base");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/TestDemo.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Test method");
+	}
+
+}
